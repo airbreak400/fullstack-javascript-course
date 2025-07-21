7ca6292b948d2f2dcda4e23fd45ae42f3d2c26ee
souljaboy <72752454+airbreak400@users.noreply.github.com>
2025-07-22 02:59:58 +0500

    Remaining code removed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -49,17 +49,3 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
-// Practice assignment Variable name and Data type
-
-// let country = 'Uzbekistan';
-// let continent = 'Central Asia';
-// let population = 33700000;
-
-// let isIsland = false; 
-// let language;
-
-// console.log(typeof country);
-// console.log(typeof continent);
-// console.log(typeof population);
-// console.log(typeof isIsland);
-// console.log(typeof language);
