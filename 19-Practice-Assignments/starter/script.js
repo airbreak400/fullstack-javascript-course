fd25259e1ea24b58529ee4ef9b7e0ca38f33dda2
souljaboy <72752454+airbreak400@users.noreply.github.com>
2025-07-22 02:58:24 +0500

    folder for practice assignments

---
+++ 19-Practice-Assignments/starter/script.js
@@ -0,0 +1,14 @@
+// Practice assignment Variable name and Data type
+
+let country = 'Uzbekistan';
+let continent = 'Central Asia';
+let population = 33700000;
+
+let isIsland = false; 
+let language;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
