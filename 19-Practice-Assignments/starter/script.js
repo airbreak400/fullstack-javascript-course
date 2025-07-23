ff21e6922a9b03aa25d123e8fb7ad64f15a0aa1b
souljaboy <72752454+airbreak400@users.noreply.github.com>
2025-07-23 06:06:21 +0500

    Basic Operators practice assignment

--- 19-Practice-Assignments/starter/script.js
+++ 19-Practice-Assignments/starter/script.js
@@ -3,9 +3,15 @@
 const country = 'Uzbekistan';
 const continent = 'Central Asia';
 let population = 33700000;
-
+let averagePopulation = 33000000;
+let finlandPopulation = 6000000;
 const isIsland = false; 
 const language = 'Uzbek';
+console.log(country + " " + "is in " + continent + ", and its " + population + " " + "people speak"+ " " + language);
+console.log(population * 2);
+console.log(population / 2);
+console.log(population < averagePopulation);
+console.log(population > finlandPopulation);
 
 console.log(typeof country);
 console.log(typeof continent);
