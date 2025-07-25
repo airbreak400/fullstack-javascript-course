543262b47c342f0c987d824a06b209d8a20d8819
souljaboy <72752454+airbreak400@users.noreply.github.com>
2025-07-25 16:33:54 +0500

    Taking Decisions if else Statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -108,7 +108,7 @@ console.log(x, y);
 const averageAge =  (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
 */
-
+/*
 const firstName = 'Jonas';
 const job = 'Teacher';
 const birthYear = 1991; 
@@ -127,3 +127,23 @@ lines');
 console.log(`String 
 multiple 
 lines`);
+*/
+
+/*
+//Taking Decisions if else statements
+const age = 15;
+if(age >= 18) {
+    console.log('Sarah can start driving license');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)
+}
+const birthYear = 1999;
+let century;
+if(birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+*/
