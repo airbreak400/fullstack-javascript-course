3c8b91420456145554cd87df14aadfe60f4b5720
souljaboy <72752454+airbreak400@users.noreply.github.com>
2025-07-27 00:21:22 +0500

    Type Conversion and Coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -147,3 +147,23 @@ if(birthYear <= 2000) {
 }
 console.log(century);
 */
+/*
+// Type Conversion 
+const inputYear = '1991';
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// Type Coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
+*/
