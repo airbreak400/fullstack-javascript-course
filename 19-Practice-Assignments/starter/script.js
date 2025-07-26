b3a32b4e23cd61c66b3f3a200b3cb9a674ea0e87
souljaboy <72752454+airbreak400@users.noreply.github.com>
2025-07-27 00:21:45 +0500

    Type conversion and coercion practice

--- 19-Practice-Assignments/starter/script.js
+++ 19-Practice-Assignments/starter/script.js
@@ -64,3 +64,10 @@ if(bmiMark > bmiJohn){
 } else {
    console.log(`John's BMI (${bmiJohn.toFixed(1)}) is higher than Mark's (${bmiMark.toFixed(1)})!`)
 }
+
+// Type Conversion and Coercion practice
+'9' - '5'; // 4
+'19' - '13' + '17'; // '617'
+'19' - '13' + 17; // 23
+'123' < 57; // false
+5 + 6 + '4' + 9 - 4 - 2; // 1143
